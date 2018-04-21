d0fbae09c52355c4a44b79b9da2ca25da4ac9bcb
MinhCT <minhct1997@gmail.com>
2018-04-21 08:36:33 +0700

    Merged files

--- src/main/java/com/hackathon/kitty/gamification/controllers/KittyController.java
+++ src/main/java/com/hackathon/kitty/gamification/controllers/KittyController.java
@@ -2,10 +2,7 @@ package com.hackathon.kitty.gamification.controllers;
 
 import com.hackathon.kitty.gamification.models.Kitty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.hackathon.kitty.gamification.repository.KittyRepository;
 
 import java.util.List;
