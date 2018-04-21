aaf8d52d9e94da8b4cd4b8b204cbb6862d79f6af
LamPD1 <lampd1@fsoft.com.vn>
2018-04-21 10:01:01 +0700

    Update Swagger

--- src/main/java/com/hackathon/kitty/gamification/GamificationApplication.java
+++ src/main/java/com/hackathon/kitty/gamification/GamificationApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class GamificationApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(GamificationApplication.class, args);
     }
 }
