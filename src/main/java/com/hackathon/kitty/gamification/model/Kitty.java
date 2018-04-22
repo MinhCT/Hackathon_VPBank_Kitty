e442a1fb132118fae16e316a0b1284047c4d71c4
MinhCT <minhct1997@gmail.com>
2018-04-22 08:59:30 +0700

    Fixed errors

--- src/main/java/com/hackathon/kitty/gamification/model/Kitty.java
+++ src/main/java/com/hackathon/kitty/gamification/model/Kitty.java
@@ -32,6 +32,10 @@ public class Kitty implements Serializable {
     private Timestamp lastFeedDate;
     private Timestamp lastBathDate;
 
+    public Kitty() {
+        
+    }
+
     public Kitty(int userId, int kittyTypeId, String name, byte gender, String image, int price, int level, int experience, int rank, boolean deleted, int hunger, int hygiene, Timestamp lastFeedDate, Timestamp lastBathDate) {
         this.userId = userId;
         this.kittyTypeId = kittyTypeId;
